87d01f95e3eee572d09df190eb2c40e25b0681d8
regiofaissal <regiofaissalliderr@gmail.com>
2025-05-08 00:04:40 -0300

    app_ride

--- speedometer.js
+++ speedometer.js
@@ -4,12 +4,14 @@ const stopBnt = document.querySelector("#stop");
 
 
 let watchID = null;
+let currentRide = null; 
 
 startBnt.addEventListener("click", () => {
     if (watchID)
         return;
 
     function handleSuccess(position) {
+        addPosition(currentRide, position); 
         speedElement.textContent = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1) : 0;
     }
 
@@ -17,9 +19,8 @@ startBnt.addEventListener("click", () => {
         console.log(error.msg);
     }
     const options = {enableHighAccuracy: true};
-
+    currentRide = criateNewRide(); 
     watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, options)
-
     startBnt.classList.add("d-none");
     stopBnt.classList.remove("d-none");
 
@@ -31,7 +32,11 @@ stopBnt.addEventListener("click", () => {
 
     navigator.geolocation.clearWatch(watchID);
     watchID = null;
+    upDateStopTime(currentRide); 
+    currentRide = null; 
     startBnt.classList.remove("d-none");
     stopBnt.classList.add("d-none");
+
+    window.location.href = "index.html";
 
 })
